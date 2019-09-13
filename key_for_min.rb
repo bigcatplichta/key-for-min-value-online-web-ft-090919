78f21166d9e6428e80d12c678df32ef2945d2ac6
Matt Plichta <bigcatplichta@gmail.com>
2019-09-13 16:40:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,9 @@
 
 def key_for_min_value(name_hash)
   min_key = ""
-  min_value = 1000000000  
+   
   name_hash.each do |key, value|
+    min_value = value if !min_value
     if value <= min_value
       min_value = value 
       min_key = key 
